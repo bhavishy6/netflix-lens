3cfb20d9a36255a16c9b0e259af9db88eef6dead
Bhavish <bhavishy6@gmail.com>
2020-06-24 10:27:51 -0400

    Clean up of detected WOI, empty list when the WOI are stale

--- background.js
+++ background.js
@@ -12,7 +12,10 @@ function connected(p) {
     let organizations = doc.organizations().match('(@isTitleCase)+').data().map(function(a) { return a.text.trim(); });
     let people = doc.people().match('(@isTitleCase)+').data().map(function(a) { return a.text.trim(); });
     let places = doc.places().match('(@isTitleCase)+').data().map(function(a) { return a.text.trim(); });
-    let capitals = doc.match('(@isTitleCase)+').if('#Noun').data().map(function(a) { return a.text.trim(); });
+    let capitals = doc.match('(@isTitleCase)+').if('#ProperNoun').data().map(function(a) { return a.text.trim(); });
+    //https://observablehq.com/@spencermountain/topics-named-entity-recognition
+    //https://observablehq.com/@spencermountain/compromise-match-syntax
+    //https://observablehq.com/@spencermountain/compromise-match
 
     portFromCS.postMessage({
       sub: subs,
@@ -27,3 +30,8 @@ function connected(p) {
 }
 
 browser.runtime.onConnect.addListener(connected);
+
+
+function searchFandom(){
+  
+}
--- netflix-lens.js
+++ netflix-lens.js
@@ -8,8 +8,10 @@ var allPlaces = [];
 var allTopics = [];
 var allCapitals = [];
 
+//must be on the watch screen
+// var title = document.getElementsByClassName("ellipsize-text");
 
-
+// browser.runtime.sendMessage();
 if(document.getElementById('netflix-lens-panel') == null) {
 
     var overlay = document.createElement('div');
@@ -95,31 +97,68 @@ console.log("Found Topics: " +m.topics);
     if(m.organizations.length > 0) {
         Array.prototype.push.apply(allOrganizations, m.organizations); 
         m.organizations.forEach(org => {
-            addTopic(org, "organizations");
+            organization = org.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ");
+
+            if(!allOrganizations.includes(organization)){
+                allOrganizations.push(organization);
+                addTopic(org, "organizations");
+
+                if(allOrganizations.length > 20) {
+                    allOrganizations.splice(0, allOrganizations.length - 2);
+                    removeAllChildNodes("organizations");
+                }
+            }
         });
     }
 
     if(m.people.length > 0) {
-        Array.prototype.push.apply(allPeople, m.people);
-        m.people.forEach(person => {
-            addTopic(person, "people");
+        m.people.forEach(per => {
+            person = per.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ");
+
+            if(!allPeople.includes(person)) {
+                allPeople.push(person);
+                addTopic(person, "people");
+
+                if(allPeople.length > 20) {
+                    allPeople.splice(0, allPeople.length - 2);
+                    removeAllChildNodes("people");
+                }
+            }
         });
         
     }
     if(m.places.length > 0) {
-        Array.prototype.push.apply(allPlaces, m.places);
-        m.places.forEach(place => {
-            addTopic(place, "places");
+        m.places.forEach(pl => {
+            place = pl.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ");
+
+            if(!allPlaces.includes(place)) {
+                allPlaces.push(place);
+                addTopic(place, "places");
+
+                if(allPlaces.length > 20) {
+                    allPlaces.splice(0, allPlaces.length - 2);
+                    removeAllChildNodes("places");
+                }
+            }
         });
     }
     
     console.log("Cap: " + m.capitals.toString());
 
     if(m.capitals.length > 0) {
-        Array.prototype.push.apply(allCapitals, m.capitals);
-        m.capitals.forEach(capital => {
-            console.log(capital);
-            addTopic(capital, "possible");
+        m.capitals.forEach(cap => {
+            capital = cap.replace(/[.,\/#?!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s{2,}/g," ");
+
+            if(!allCapitals.includes(capital)) {
+                allCapitals.push(capital);
+                addTopic(capital, "possible");
+
+                if(allCapitals.length > 6) {
+                    //if more than 20 inside array, remove all old ones.
+                    allCapitals.splice(0, allCapitals.length - 2);
+                    removeAllChildNodes("possible");
+                }
+            }
         });
     }
     
@@ -139,11 +178,21 @@ function addTopic(topic, category) {
     document.getElementById(category+"-section").appendChild(item);
 }
 
+function removeAllChildNodes(category) {
+    var sec = document.getElementById(category+"-section");
+    var divs = sec.getElementsByTagName("div");
+    if(divs !== null) {
+        for(var v = divs.length - 1; v >= 2; v--) {
+            sec.removeChild(divs[0]);
+        }
+    }
+}
+
 /**
  * @TODO
- * 1. remove duplicates from collected WOI
- * 2. Weed out bad 'possible' WOI
- * 3. Look up the terms
+ * 1. Wait until on the watch panel then detec title. 
+ * 2. connect to fandom api for that title (if it exists)
+ * 3. Search the fandom site for the term /api/v1/Search/List
  * 4. Make the UI better
  * 
  */
